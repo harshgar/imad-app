5fe3f1774ddb6315df8cb1db8ebc854291c056f3
harshgar <harshgar1989@gmail.com>
2017-08-06 21:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,9 @@
-console.log('Loaded!');
-// Change text of HTML 
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-
-//MOVE IMAGE
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onClick=function(){
-    var interval = setInterval(moveRight,100);
+//Counter code
+var button = document.getElementById('counter');
+var counter=0;
+buttton.onclick = function() {
   
+  counter=counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML=counter.toString();
 };
