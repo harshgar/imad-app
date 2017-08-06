68f40de1ee4131508ddb54f680769eb82e31029d
harshgar <harshgar1989@gmail.com>
2017-08-06 17:33:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
 });
