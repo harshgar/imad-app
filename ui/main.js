60a8a4dd99593b4972848ea4d16af1ab1535fc6c
harshgar <harshgar1989@gmail.com>
2017-08-06 20:36:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,16 @@ console.log('Loaded!');
 // Change text of HTML 
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//MOVE IMAGE
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
+img.onClick=function(){
+    var interval = setInterval(moveRight,100);
+  
+};
