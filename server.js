e91574f393317ef8e605b7e6423a28beecbe670b
harshgar <harshgar1989@gmail.com>
2017-08-20 11:36:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config={
+    user: 'harshgar1989', 
+    databsae: 'harshgar1989'
+    host: 'db.imad.hasura-app.io'
+    port: '5432'
+    password: process.emv.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -97,4 +105,15 @@ app.get('/counter', function (req, res) {
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+});
+
+var Pool= new Pool(config); 
+app.get('/test-db',function (req,res) {
+    pool.query('Select * from test',function (err,result) {
+     if(err){
+         res.status(500).send(err.toString());
+     }else {
+         res.send(JSON.string(fy(result)))
+     }
+ });
 });
