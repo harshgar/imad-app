a51536026ac2033d75af768c4043a33784f2b2a8
harshgar <harshgar1989@gmail.com>
2017-08-26 19:13:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ function hash(input,salt){
 
 app.get('/hash/:input',function (req,res){
     var hashString=hash(req.params.input,'this-is-some-random-string');
-    res.send(hashedString);
+    res.send(hashString);
 });
 
 app.get('/', function (req, res) {
